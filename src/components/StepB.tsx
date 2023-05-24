3089d0a4ec0ba27e7372aaae3882219ed55c177e
Trung Anh <anhtt1904@gmail.com>
2023-05-24 15:25:49 +0700

    Add components and features

---
+++ src/components/StepB.tsx
@@ -0,0 +1,128 @@
+import React, {useState} from 'react';
+
+interface StepBProps {
+    data: {
+        email: string;
+        phoneNumber: string;
+        dateOfBirth: string;
+    };
+
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onBack: () => void;
+    onNext: () => void;
+}
+
+interface Errors {
+    email: string,
+    phoneNumber: string,
+    dateOfBirth: string,
+}
+
+const errorsCopy: Errors = {
+    email: '',
+    phoneNumber: '',
+    dateOfBirth: '',
+};
+
+const StepB: React.FC<StepBProps> = ({ data,onChange, onBack, onNext }) => {
+    const [errors, setErrors] = useState<Errors>(errorsCopy);
+
+    // For simplify setting up project, I create simple validation. In realife we can use libraries for doing validation
+    // such as Formik
+
+    const validateEmail = (email: string) => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailRegex.test(email);
+    };
+
+    const validateDateOfBirth = (dateOfBirth: string) => {
+        // Assuming `dateOfBirth` is in the format "YYYY-MM-DD"
+        const yearOfBirth = parseInt(dateOfBirth.substr(0, 4));
+        return yearOfBirth < 2010;
+    };
+
+    const validatePhoneNumber = (phoneNumber: string) => {
+        // Phone number validation using regular expression
+        const phoneRegex = /^\d{10}$/;
+        return phoneRegex.test(phoneNumber);
+    };
+    const validateStepB = () => {
+        let isValid = true;
+        setErrors({...errorsCopy});
+        const { email, phoneNumber, dateOfBirth } = data;
+
+        if (email.trim() === '') {
+            setErrors(prev => ({ ...prev, email : 'Email is required' }));
+            isValid = false;
+        } else if (!validateEmail(email)) {
+            setErrors(prev => ({ ...prev, email : 'Invalid email format' }));
+            isValid = false;
+        }
+
+        if (phoneNumber.trim() === '') {
+            setErrors(prev => ({ ...prev, phoneNumber : 'Phone Number is required' }));
+            isValid = false;
+        } else if (!validatePhoneNumber(phoneNumber)) {
+            setErrors(prev => ({ ...prev, phoneNumber : 'Invalid phone number format (10 decimal characters)' }));
+            isValid = false;
+        }
+
+        if (dateOfBirth.trim() === '') {
+            setErrors(prev => ({ ...prev, dateOfBirth : 'Date of Birth is required' }));
+            isValid = false;
+        } else if (!validateDateOfBirth(dateOfBirth)) {
+            setErrors(prev => ({ ...prev, dateOfBirth : 'Date of Birth must be earlier than 2010' }));
+            isValid = false;
+        }
+
+        isValid && onNext();
+    };
+    return (
+        <div className="mb-6">
+            <h2 className="text-2xl font-bold mb-4">Step B - Additional Information</h2>
+            <div className="mb-4">
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email Address"
+                    className="border rounded p-2"
+                    value={data.email}
+                    onChange={onChange}
+                />
+                {errors.email && <p className="text-red-500">{errors.email}</p>}
+            </div>
+            <div className="mb-4">
+                <input
+                    type="tel"
+                    name="phoneNumber"
+                    placeholder="Phone Number"
+                    className="border rounded p-2"
+                    value={data.phoneNumber}
+                    onChange={onChange}
+                />
+                {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
+            </div>
+            <div>
+                <input
+                    type="date"
+                    name="dateOfBirth"
+                    placeholder="Date of Birth"
+                    className="border rounded p-2"
+                    value={data.dateOfBirth}
+                    onChange={onChange}
+                />
+                {errors.dateOfBirth && <p className="text-red-500">{errors.dateOfBirth}</p>}
+            </div>
+            <div className="mt-6">
+                <button className="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded  mr-2" onClick={onBack}>
+                    Back
+                </button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={validateStepB}>
+                    Next
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default StepB;
